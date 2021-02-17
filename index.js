69a7c06170bce38b56e654d0c783e8b9d3499b77
MMiirraa <gmppb@ya.ru>
2021-02-17 10:31:57 +0300

    codewares.

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let car = {
+    cost: 100000
+}
+// if (car.cost > 1000000){
+//     console.log('Автомобиль дорогой')
+// }
+// else{
+//     console.log('car is chip')
+// }
+car.cost > '1000000' ? console.log('expensiv') : console.log('chip');
